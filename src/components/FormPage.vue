d09b854bbca70f9bc29ba195a2eaef18299d4e29
fatihcoskun41 <39034887+fatihcoskun41@users.noreply.github.com>
2021-01-18 00:32:01 +0300

    Add files via upload

---
+++ src/components/FormPage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <b-form @submit="onSubmit" v-if="show">
+      <b-form-group id="input-group-2"  label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="title"
+          placeholder="Enter title"
+          required
+        ></b-form-input>
+      </b-form-group>
+
+      <b-form-group>
+        <b-form-textarea
+          id="textarea"
+          v-model="textarea"
+          placeholder="Enter something..."
+          rows="3"
+          max-rows="6"
+        ></b-form-textarea>
+      </b-form-group>
+      <b-form-group>
+        <b-form-input v-model="imageUrl" placeholder="Resim Linkini Giriniz"></b-form-input>
+      </b-form-group>
+     
+
+      <b-button type="submit" variant="primary">Kaydet</b-button>
+    </b-form>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      
+        textarea: "",
+        title: "New Title", 
+         imageUrl:"",
+    
+
+      show: true,
+    };
+  },
+  methods: {
+    onSubmit(event) {
+      event.preventDefault();
+      const a =[this.textarea,this.title,this.imageUrl]
+      this.$emit('data',a)
+      
+ 
+    },
+  
+    
+  },
+};
+</script>
---
+++ src/components/ListPage.vue
@@ -0,0 +1,39 @@
+<template>
+
+   <div>
+   <h4>{{title}}</h4>
+
+
+    <b-card-group  deck>
+      <b-card>
+          <img style="height: 300px; width:95%" :src="url">
+        <b-card-text>
+         {{textarea}}
+        </b-card-text>
+      
+</b-card>
+     
+    </b-card-group>
+    <br>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+        
+        
+        }
+    },
+    props:["title","textarea","url"]
+
+
+    
+
+}
+</script>
+
+<style>
+
+</style>
